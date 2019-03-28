5fb82389a7a7a8344dd1a11bf80c6aaa40dee4cd
George-Flores <george.flores@laureate.net>
2019-03-28 14:26:18 -0400

    correct location 2

--- js/scripts.js
+++ js/scripts.js
@@ -237,7 +237,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var itc_kol = {lat: 25.8099397, lng: 109.0013736};
+    var itc_kol = {lat: 25.8099397, lng: -109.0013736};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: itc_kol,
